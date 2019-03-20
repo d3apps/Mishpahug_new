72ac626f5396a9765e30067f74faff4954f40993
Dennis Davydov <dennis7807@gmail.com>
2019-03-20 19:50:53 +0200

    added int

--- app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
+++ app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int number =10;
+    int anInt = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
