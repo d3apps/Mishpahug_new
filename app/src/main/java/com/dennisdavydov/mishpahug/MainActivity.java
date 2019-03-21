04dce9bf8f46cf4dbfb36f4855f864a9871e4925
Dennis Davydov <dennis7807@gmail.com>
2019-03-21 20:23:43 +0200

    added temporary button next, designed main activity

--- app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
+++ app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
@@ -3,9 +3,13 @@ package com.dennisdavydov.mishpahug;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button nextBtn;
 
 
     @Override
@@ -13,10 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent=new Intent(this,MainPage.class);
-        startActivity(intent);
+        nextBtn = findViewById(R.id.nextBtn);
+        nextBtn.setOnClickListener(this);
+
+
 
     }
 
 
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(this,MainPage.class);
+        startActivity(intent);
+        finish();
+    }
 }
