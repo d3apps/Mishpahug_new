0029a7affae223571d0dacc5c9b92f7d277d0d65
Dennis Davydov <dennis7807@gmail.com>
2019-03-21 21:51:38 +0200

    moved fonts to assets, added SingletonFonts

--- app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
+++ app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button nextBtn;
+    TextView main_screen_header, main_screen_text, main_screen_text2;
 
 
     @Override
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         nextBtn = findViewById(R.id.nextBtn);
         nextBtn.setOnClickListener(this);
+
+        main_screen_header = findViewById(R.id.main_screen_header);
+        main_screen_header.setTypeface(SingletonFonts.getInstance(this).getFont5());
+
+        main_screen_text = findViewById(R.id.main_screen_text);
+        main_screen_text.setTypeface(SingletonFonts.getInstance(this).getFont4());
+
+        main_screen_text2 = findViewById(R.id.main_screen_text2);
+        main_screen_text2.setTypeface(SingletonFonts.getInstance(this).getFont4());
 
 
 
---
+++ app/src/main/java/com/dennisdavydov/mishpahug/SingletonFonts.java
@@ -0,0 +1,86 @@
+package com.dennisdavydov.mishpahug;
+
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+public class SingletonFonts {
+    private static Typeface font1;
+    private static Typeface font2;
+    private static Typeface font3;
+    private static Typeface font4;
+    private static Typeface font5;
+
+    private Typeface getFont1() {
+        return font1;
+    }
+
+    private  Typeface getFont2() {
+        return font2;
+    }
+
+    private Typeface getFont3() {
+        return font3;
+    }
+
+    private Typeface getFont4() {
+        return font4;
+    }
+    private Typeface getFont5() {
+        return font5;
+    }
+
+
+    private static void setFont1(Typeface font1) {
+        SingletonFonts.font1 = font1;
+    }
+
+    private static void setFont2(Typeface font2) {
+        SingletonFonts.font2 = font2;
+    }
+
+    private static void setFont3(Typeface font3) {
+        SingletonFonts.font3 = font3;
+    }
+
+    private static void setFont4(Typeface font4) {
+        SingletonFonts.font4 = font4;
+    }
+
+    private static void setFont5(Typeface font5) {
+        SingletonFonts.font5 = font5;
+    }
+
+    private static volatile SingletonFonts instance;
+
+    private SingletonFonts() {}
+
+    private static SingletonFonts getInstance(Context activity) {
+        SingletonFonts localInstance = instance;
+        if (localInstance == null) {
+            synchronized (SingletonFonts.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new SingletonFonts();
+                }
+            }
+            setFont1(Typeface.createFromAsset(activity.getAssets(),
+                    "fonts/raleway_extralight.ttf"));
+            setFont2(Typeface.createFromAsset(activity.getAssets(),
+                    "fonts/raleway_light.ttf"));
+            setFont3(Typeface.createFromAsset(activity.getAssets(),
+                    "fonts/raleway_medium.ttf"));
+            setFont4(Typeface.createFromAsset(activity.getAssets(),
+                    "fonts/raleway_regular.ttf"));
+            setFont5(Typeface.createFromAsset(activity.getAssets(),
+                    "fonts/raleway_semibold.ttf"));
+
+
+
+
+        }
+        return localInstance;
+    }
+
+
+}
