b6c7c2a1bc50c93c84d13cd64bc5d847758619ff
VitBoj <vitboj22@gmail.com>
2019-03-21 10:16:03 +0200

    добавил главную страницу и тд

--- app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
+++ app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
@@ -1,15 +1,22 @@
 package com.dennisdavydov.mishpahug;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    int number =10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent=new Intent(this,MainPage.class);
+        startActivity(intent);
+
     }
+
+
 }
