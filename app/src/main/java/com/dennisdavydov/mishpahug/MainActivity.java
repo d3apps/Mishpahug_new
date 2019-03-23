24153095a26273e8d925dc72013181cd2626db58
Dennis Davydov <dennis7807@gmail.com>
2019-03-24 00:27:35 +0200

    added registation fragment

--- app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
+++ app/src/main/java/com/dennisdavydov/mishpahug/MainActivity.java
@@ -1,18 +1,31 @@
 package com.dennisdavydov.mishpahug;
 
+
+
 import android.content.Intent;
+import android.net.Uri;
+import android.support.constraint.ConstraintLayout;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import com.dennisdavydov.mishpahug.fragments.RegFragment;
 import com.dennisdavydov.mishpahug.singletons.SingletonFonts;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        RegFragment.OnFragmentInteractionListener {
 
-    Button nextBtn;
+    Button nextBtn, loginRegBtn;
     TextView main_screen_header, main_screen_text, main_screen_text2;
+    RegFragment regFragment;
+    FragmentManager manager;
+    FragmentTransaction transaction;
+    ConstraintLayout innerConstraintMainActivity;
 
 
     @Override
@@ -23,6 +36,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         nextBtn = findViewById(R.id.nextBtn);
         nextBtn.setOnClickListener(this);
 
+        loginRegBtn = findViewById(R.id.loginRegBtn);
+        loginRegBtn.setOnClickListener(this);
+
         main_screen_header = findViewById(R.id.main_screen_header);
         main_screen_header.setTypeface(SingletonFonts.getInstance(this).getFont5());
 
@@ -32,15 +48,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         main_screen_text2 = findViewById(R.id.main_screen_text2);
         main_screen_text2.setTypeface(SingletonFonts.getInstance(this).getFont4());
 
-
+        innerConstraintMainActivity
+                = findViewById(R.id.innerConstraintMainActivity);
 
     }
 
 
     @Override
     public void onClick(View v) {
-        Intent intent=new Intent(this,MainPage.class);
-        startActivity(intent);
-        finish();
+        switch (v.getId()){
+            case R.id.nextBtn:{
+                Intent intent=new Intent(this,MainPage.class);
+                startActivity(intent);
+                finish();
+            }
+            break;
+            case R.id.loginRegBtn:{
+                Toast.makeText(this,"Button pressed",Toast.LENGTH_LONG).show();
+                innerConstraintMainActivity.setVisibility(View.GONE);
+                manager = getSupportFragmentManager();
+                transaction = manager.beginTransaction();
+                regFragment = new RegFragment();
+                transaction.add(R.id.frameForFragment,regFragment);
+                transaction.commit();
+            }
+            break;
+        }
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
