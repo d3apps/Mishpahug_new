c46fa4312b685b3db66a9581cec144068eb4e7e2
Dennis Davydov <dennis7807@gmail.com>
2019-03-28 21:44:03 +0200

    ///

--- app/src/main/java/com/dennisdavydov/mishpahug/App.java
+++ app/src/main/java/com/dennisdavydov/mishpahug/App.java
@@ -2,5 +2,31 @@ package com.dennisdavydov.mishpahug;
 
 import android.app.Application;
 
+import com.dennisdavydov.mishpahug.providers.ApiProvider;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class App extends Application {
+
+    private static final String BASE_URL = "http://www.mocky.io";
+    private static ApiProvider provider;
+    private Retrofit retrofit;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        provider = retrofit.create(ApiProvider.class);
+
+    }
+
+    public static ApiProvider getProvider(){
+        return provider;
+    }
+
 }
--- app/src/main/java/com/dennisdavydov/mishpahug/providers/ApiProvider.java
+++ app/src/main/java/com/dennisdavydov/mishpahug/providers/ApiProvider.java
@@ -1,4 +1,15 @@
 package com.dennisdavydov.mishpahug.providers;
 
-interface ApiProvider {
+import com.dennisdavydov.mishpahug.models.EventsDescription;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiProvider {
+
+    @GET("/v2/5c9d172c33000056003f227e")
+    Call<EventsDescription> getEvent();
+
+
 }
