2bae053388d1c49efb78caad2fbd9fd173f7a6a1
VitBoj <vitboj22@gmail.com>
2019-04-05 07:36:00 +0200

    eventInfoFragment

--- app/src/main/java/com/dennisdavydov/mishpahug/fragments/EventInfoFragment.java
+++ app/src/main/java/com/dennisdavydov/mishpahug/fragments/EventInfoFragment.java
@@ -3,10 +3,15 @@ package com.dennisdavydov.mishpahug.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dennisdavydov.mishpahug.R;
 
@@ -24,6 +29,12 @@ public class EventInfoFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    ImageView UserImg;
+    TextView  holiday;
+    TextView day;
+    TextView eventDescription;
+    Button joinToFamily;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -34,14 +45,7 @@ public class EventInfoFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventInfoFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static EventInfoFragment newInstance(String param1, String param2) {
         EventInfoFragment fragment = new EventInfoFragment();
@@ -92,18 +96,19 @@ public class EventInfoFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        holiday=view.findViewById(R.id.eventInfoHoliday);
+        day=view.findViewById(R.id.eventInfoDate);
+        eventDescription=view.findViewById(R.id.eventDescription);
+        UserImg=view.findViewById(R.id.eventInfoImage);
+        joinToFamily=view.findViewById(R.id.joinFamilyBtn);
+
     }
 }
